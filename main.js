e356421ece7eb9f0640abcaa0494acb198d85bc9
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-10-28 16:47:57 +0100

    this

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
     //uso la funzione each per selezionare uno alla volta i drop menu
     $('.drop-menu').each(function(){
         //entro col mouse su drop-menu
-        $('.drop-menu').mouseenter(function(){
-            $(this).addClass('active');
+        $(this).mouseenter(function(){
+            $('.dropdown').addClass('active');
         });
         $('.head-right>ul>li').mouseleave(function(){
             $('.dropdown').removeClass('active');
