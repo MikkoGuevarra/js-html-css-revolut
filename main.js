6c758506bea4bb3c1c699e571ef53048fbeb4a5c
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-10-28 16:42:49 +0100

    used 'this' but not working

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
-    $('.drop-menu').mouseenter(function(){
-        $('.dropdown').addClass('active');
-    });
-    $('.head-right>ul>li').mouseleave(function(){
-        $('.dropdown').removeClass('active');
-    });
 
-
+    //uso la funzione each per selezionare uno alla volta i drop menu
+    $('.drop-menu').each(function(){
+        //entro col mouse su drop-menu
+        $('.drop-menu').mouseenter(function(){
+            $(this).addClass('active');
+        });
+        $('.head-right>ul>li').mouseleave(function(){
+            $('.dropdown').removeClass('active');
+        });
+    });
 });
