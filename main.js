624b58467e7f0a974cb8816333dad5f2d24158d8
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-10-29 09:23:58 +0100

    tried w/out using each

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 $(document).ready(function(){
 
     //uso la funzione each per selezionare uno alla volta i drop menu
-    $('.drop-menu').each(function(){
-        //entro col mouse su drop-menu
-        $(this).mouseenter(function(){
-            $(this).children('.dropdown').addClass('active');
-        });
+    // $('.drop-menu').each(function(){
+    //     //entro col mouse su drop-menu
+    //     $(this).mouseenter(function(){
+    //         $(this).children('.dropdown').addClass('active');
+    //     });
+    //
+    // });
+    //
+    // $('.head-right>ul>li').mouseleave(function(){
+    //     $('.dropdown').removeClass('active');
+    // });
 
+    //second method
+    //
+    // $('.drop-menu').mouseenter(function(){
+    //     $(this).find('.dropdown').addClass('active');
+    // });
+    //
+    // $('.drop-menu').mouseleave(function(){
+    //     $(this).find('.dropdown').removeClass('active');
+    // });
+
+    //third method without using each
+
+    $('.drop-menu').mouseenter(function(){
+        $(this).children('.dropdown').addClass('active');
     });
 
     $('.head-right>ul>li').mouseleave(function(){
